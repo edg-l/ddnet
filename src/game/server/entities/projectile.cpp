9e675f75f204b1d4e1016e1f47edc26c291d1571
Ryozuki <edgar@ryobyte.com>
2018-07-14 22:50:21 +0200

    debug

--- src/game/server/entities/projectile.cpp
+++ src/game/server/entities/projectile.cpp
@@ -121,7 +121,7 @@ void CProjectile::Tick()
 
 	CCharacter *pTargetChr = nullptr;
 
-	if(pOwnerChar->m_Hit&CCharacter::DISABLE_HIT_GRENADE)
+	if(pOwnerChar && pOwnerChar->m_Hit&CCharacter::DISABLE_HIT_GRENADE)
 		GameServer()->m_World.IntersectCharacter(PrevPos, ColPos, m_Freeze ? 1.0f : 6.0f, ColPos, pOwnerChar, m_Owner);
 
 	if(Collide && pTargetChr)
